7cec77f632ad47114c6acd335b5b100d90784a66
Plopmenz <plopmenz@gmail.com>
2025-10-14 17:26:02 +0400

    history redesign

--- mini-app/components/project-history.tsx
+++ mini-app/components/project-history.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import { useRequestRequestInfo } from "@openmesh-network/xnode-manager-sdk-react";
+import axios from "axios";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+import { Spinner } from "./ui/spinner";
+import { cn } from "@/lib/utils";
 
 interface Deployment {
   id: number;
   project: string;
   instructions: string;
   submitted_at: number;
-  started_at?: number;
-  finished_at?: number;
-  deployment_request?: number;
+  coding_started_at: number | null;
+  coding_finished_at: number | null;
+  imagegen_started_at: number | null;
+  imagegen_finished_at: number | null;
+  git_hash: string | null;
+  deployment_request: number | null;
 }
 
 export function ProjectHistory({ project }: { project: string }) {
@@ -33,40 +39,88 @@ export function ProjectHistory({ project }: { project: string }) {
   });
 
   return (
-    <div className="flex flex-col gap-3 place-items-center">
+    <div className="flex w-full flex-col gap-3 place-items-center">
       <span className="text-xl">History</span>
-      {history
-        ?.sort((d1, d2) => d1.submitted_at - d2.submitted_at)
-        .map((deployment) => (
-          <HistoryItem key={deployment.id} deployment={deployment} />
-        ))}
+      <Accordion className="w-full px-1" type="single" collapsible>
+        {history
+          ?.sort((d1, d2) => d2.submitted_at - d1.submitted_at)
+          .map((deployment) => (
+            <HistoryItem key={deployment.id} deployment={deployment} />
+          ))}
+      </Accordion>
     </div>
   );
 }
 
+enum Status {
+  unknown = "...",
+  deployed = "Deployed",
+  error = "Error",
+  deploying = "Deploying",
+  imagegen = "Generating",
+  coding = "Programming",
+  queued = "Queued",
+}
 function HistoryItem({ deployment }: { deployment: Deployment }) {
-  const status = deployment.deployment_request
-    ? "done"
-    : deployment.finished_at
-    ? "deploying"
-    : deployment.started_at
-    ? "coding"
-    : "queued";
+  const { data: deploymentRequest } = useRequestRequestInfo({
+    session: {
+      axiosInstance: axios.create({
+        withCredentials: true,
+      }),
+      baseUrl: "https://miniapp-host.xnode-manager.openxai.org",
+    },
+    request_id: deployment.deployment_request ?? undefined,
+  });
+
+  const status =
+    deployment.imagegen_finished_at !== null
+      ? deploymentRequest === undefined
+        ? Status.unknown
+        : deploymentRequest.result === null
+        ? Status.deploying
+        : "Success" in deploymentRequest.result
+        ? Status.deployed
+        : "Error" in deploymentRequest.result
+        ? Status.error
+        : Status.unknown
+      : deployment.coding_finished_at !== null
+      ? Status.imagegen
+      : deployment.coding_started_at !== null
+      ? Status.coding
+      : Status.queued;
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          {new Date(deployment.submitted_at * 1000).toLocaleString()}
-        </CardTitle>
-        <CardDescription>{deployment.instructions}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex place-content-between place-items-center">
-          <span>Status:</span>
-          <span>{status}</span>
+    <AccordionItem value={deployment.id.toString()}>
+      <AccordionTrigger>
+        <div className="flex w-full place-content-between place-items-center">
+          <span>
+            {new Date(deployment.submitted_at * 1000).toLocaleString()}
+          </span>
+          {status !== Status.unknown && (
+            <div
+              className={cn(
+                "flex place-items-center gap-1 rounded-full px-2 py-1",
+                status === Status.deployed && "bg-green-600",
+                status === Status.error && "bg-red-600",
+                status === Status.deploying && "bg-amber-500",
+                status === Status.imagegen && "bg-amber-500",
+                status === Status.coding && "bg-amber-500",
+                status === Status.queued && "bg-primary text-primary-foreground"
+              )}
+            >
+              {(status === Status.coding ||
+                status === Status.imagegen ||
+                status === Status.deploying) && <Spinner />}
+              <span>{status}</span>
+            </div>
+          )}
         </div>
-      </CardContent>
-    </Card>
+      </AccordionTrigger>
+      <AccordionContent>
+        <div>
+          <span>{deployment.instructions}</span>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
