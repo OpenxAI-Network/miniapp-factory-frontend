d950b869cc7933dc1ea47e011482e92146105d4f
Plopmenz <plopmenz@gmail.com>
2025-10-16 12:48:13 +0400

    add json input for farcaster link

--- mini-app/components/account-association.tsx
+++ mini-app/components/account-association.tsx
@@ -15,6 +15,8 @@ import {
 import { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import { Textarea } from "./ui/textarea";
 
 export function AccountAssociation({ project }: { project: string }) {
   const { isInMiniApp } = useMiniAppContext();
@@ -23,6 +25,7 @@ export function AccountAssociation({ project }: { project: string }) {
   const [header, setHeader] = useState<string>("");
   const [payload, setPayload] = useState<string>("");
   const [signature, setSignature] = useState<string>("");
+  const [json, setJson] = useState<string>("");
 
   if (!isInMiniApp) return <></>;
 
@@ -38,38 +41,66 @@ export function AccountAssociation({ project }: { project: string }) {
             Link this mini app with your Farcaster account.
           </DialogDescription>
         </DialogHeader>
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-1">
-            <Label htmlFor="farcaster-header">Header</Label>
-            <Input
-              id="farcaster-header"
-              value={header}
-              onChange={(e) => setHeader(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <Label htmlFor="farcaster-payload">Payload</Label>
-            <Input
-              id="farcaster-payload"
-              value={payload}
-              onChange={(e) => setPayload(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <Label htmlFor="farcaster-signature">Signature</Label>
-            <Input
-              id="farcaster-signature"
-              value={signature}
-              onChange={(e) => setSignature(e.target.value)}
-            />
-          </div>
-        </div>
+        <Tabs defaultValue="manual">
+          <TabsList>
+            <TabsTrigger value="manual">Manual</TabsTrigger>
+            <TabsTrigger value="json">JSON</TabsTrigger>
+          </TabsList>
+          <TabsContent value="manual">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
+                <Label htmlFor="farcaster-header">Header</Label>
+                <Input
+                  id="farcaster-header"
+                  value={header}
+                  onChange={(e) => setHeader(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <Label htmlFor="farcaster-payload">Payload</Label>
+                <Input
+                  id="farcaster-payload"
+                  value={payload}
+                  onChange={(e) => setPayload(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <Label htmlFor="farcaster-signature">Signature</Label>
+                <Input
+                  id="farcaster-signature"
+                  value={signature}
+                  onChange={(e) => setSignature(e.target.value)}
+                />
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="json">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
+                <Label htmlFor="farcaster-json">JSON</Label>
+                <Textarea
+                  className="max-w-full"
+                  id="farcaster-json"
+                  value={json}
+                  onChange={(e) => setJson(e.target.value)}
+                />
+              </div>
+            </div>
+          </TabsContent>
+        </Tabs>
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="secondary">Cancel</Button>
           </DialogClose>
           <Button
             onClick={() => {
+              const account_association = json
+                ? JSON.parse(json).accountAssociation
+                : {
+                    header,
+                    payload,
+                    signature,
+                  };
               fetch("/api/factory/project/account_association", {
                 method: "POST",
                 headers: {
@@ -78,17 +109,13 @@ export function AccountAssociation({ project }: { project: string }) {
                 },
                 body: JSON.stringify({
                   project,
-                  account_association: {
-                    header,
-                    payload,
-                    signature,
-                  },
+                  account_association,
                 }),
               })
                 .then(() => setOpen(false))
                 .catch(console.error);
             }}
-            disabled={!header || !payload || !signature}
+            disabled={(!header || !payload || !signature) && !json}
           >
             Link
           </Button>
