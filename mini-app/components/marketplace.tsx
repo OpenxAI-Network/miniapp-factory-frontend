8fd2ebe3316f4107e29aafcfc1d40cc887b2bfab
Plopmenz <plopmenz@gmail.com>
2025-11-20 13:02:32 +0100

    update featured

--- mini-app/components/marketplace.tsx
+++ mini-app/components/marketplace.tsx
@@ -7,6 +7,8 @@ import { ScrollArea } from "./ui/scroll-area";
 import { ProjectShowcase } from "./project-showcase";
 import { ViewMiniApp } from "./view-miniapp";
 
+const featured: string[] = [];
+
 export function Marketplace() {
   const { data: projects } = useQuery({
     queryKey: ["showcase_projects"],
@@ -36,22 +38,21 @@ export function Marketplace() {
       <div className="flex flex-col gap-1 mt-8 ml-4 w-full max-w-[500px]">
         <span className="text-4xl font-semibold">Marketplace</span>
       </div>
-      <div className="flex flex-col gap-1 mt-4 w-full max-w-[500px]">
-        <span className="ml-4 text-lg font-semibold">Featured</span>
-        <div className="grid grid-cols-3 gap-3">
-          {projects
-            ?.toReversed()
-            .slice(0, 3)
-            .map((project) => (
+      {featured.length > 0 && (
+        <div className="flex flex-col gap-1 mt-4 w-full max-w-[500px]">
+          <span className="ml-4 text-lg font-semibold">Featured</span>
+          <div className="grid grid-cols-3 gap-3">
+            {featured.map((project) => (
               <ViewMiniApp
-                key={project.id}
-                miniapp={`https://${project.name}.miniapp-factory.marketplace.openxai.network/`}
+                key={project}
+                miniapp={`https://${project}.miniapp-factory.marketplace.openxai.network/`}
               >
-                <ProjectShowcase project={project.name} />
+                <ProjectShowcase project={project} />
               </ViewMiniApp>
             ))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="flex flex-col gap-1 mt-4 w-full max-w-[500px] grow">
         <span className="ml-4 text-lg font-semibold">Latest</span>
         <ScrollArea className="w-full grow h-0 px-3">
